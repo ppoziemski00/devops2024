45c9b21955675d40b739b7fd501ab56a3514eefc
ppoziemski00 <161339787+ppoziemski00@users.noreply.github.com>
2024-06-05 00:10:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def calculate_bmi():
         return jsonify(error="Invalid data type"), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
